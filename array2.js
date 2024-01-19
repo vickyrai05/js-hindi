c07f9250fd0b9a7b0ff318a85416b8d5b93392c6
vickyrai05 <vickyrai9592989487@gmail.com>
2024-01-19 22:12:44 +0530

    etr

--- array2.js
+++ array2.js
@@ -5,16 +5,25 @@ const dc = ["superman", 'flash', 'batmen']
 
 // const all= marvel_heroes.concat(dc)
 
- const all = [...marvel_heroes,...dc]
+const all = [...marvel_heroes, ...dc]
 
-   const number =[1,2,3,[5,6,[7,8,9]]]
+const number = [1, 2, 3, [5, 6, [7, 8, 9]]]
 
-   const number1=number.flat(2);
+const number1 = number.flat(2);
 
 
-    
-console.log(Array.isArray('Vicky'));
+const one = ["vicky"]
+console.log(Array.isArray(one));
 
-console.log(Array.from('Vicky'));
+const Two = "Vicky";
+const fins = Array.from(Two).map(item => item.toUpperCase())
 
-console.log(Array.from({name:"Vicky"}));
+console.log(fins); // Output: 'VICKY'
+
+console.log(Array.from({ name: "Vicky" }));//imteresting
+
+let score1 = 78;
+let score2 = 888;
+let score3 = 4458;
+
+console.log(Array.of(score1,score2,score3));
